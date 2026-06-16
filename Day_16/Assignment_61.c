5d498ba55bf073ea45bb4b820daed5573d9503ed
Aryan Kumar <aryan11kumar10@gmail.com>
2026-06-16 22:29:38 +0530

    Update Assignment_61.c

--- Day_16/Assignment_61.c
+++ Day_16/Assignment_61.c
@@ -3,10 +3,10 @@
 int main()
 {
     int n,i, s = 0;
-    printf("enter the max number(n)-\n");
+    printf("enter the max number(n) : ");
     scanf("%d", &n);
-    int a[n-1];
-    printf("enter the elements of array\n",n-1);
+    int a[n];
+    printf("enter the elements of array : ");
     for (i = 0; i < n-1; i++)
     {
         scanf("%d", &a[i]);
